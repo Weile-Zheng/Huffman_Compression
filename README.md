245500e120444d516a158931551f61326c9dc55b
Weile <wzheng13@u.rochester.edu>
2023-03-04 18:57:05 -0500

    Commit #15 Updated huffman bash file, java file. Fixed file not found and directory issues by making inputs as absolute path

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Huffman Compression
 ***
 
+Huffman Compression Command for zsh
+
 Get Started:
 
 ```Bash
 cd ~
-fetch [] ./huffmanCompression
+git clone https://github.com/Weile-Zheng/Huffman_Compression.git
 echo export PATH=$PATH:$(pwd)/huffmanCompression" >> .zprofile
 ```
+
+Usage
+```
+huffman -[compress/decompress] <InputFile> (Optional) -o [OutputFileName]
+#Ex:
+huffman -compress text.tx
+```
+
--- src/HuffmanMain.java
+++ src/HuffmanMain.java
@@ -4,16 +4,15 @@ import java.util.*;
 
 public class HuffmanMain implements Huffman {
     private Queue<HuffmanNode> queue;
-    private Map<Integer, String> huffCodeMap; //Stores unicode char as key and huffman encoding as string value.
-    private Map<Integer, Integer> frequency; //Stores Unicode char as key and frequency as value.
-
+    private Map<Integer, String> huffCodeMap; //byte to huffman code
+    private Map<Integer, Integer> frequency; //byte to frequency
+    private int totalOutputBits = 0;
     HuffmanMain() {
         queue = new PriorityQueue<>();
         huffCodeMap = new HashMap<>();
         frequency = new HashMap<>();
     }
 
-    //Scan frequency of character in a file. Store as key value pair in hashmap.
     private void scanFrequency(String inputFile) {
         File file = new File(inputFile);
         try {
@@ -21,26 +20,23 @@ public class HuffmanMain implements Huffman {
             int s = reader.read();
             while (s != -1) {
                 if (frequency.containsKey(s))
-                    frequency.put(s, frequency.get(s) + 1);
+                    frequency.put(s, frequency.get(s) + 1); //If already contains, add one to freq
                 else {
-                    frequency.put(s, 1);
+                    frequency.put(s, 1); //Add key(0-255 decimal representation) to hashmap if not seen, freq =1
                 }
                 s = reader.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    //Given an inputfile name and outputfile name(freqfile), write frequency to output
     private void writeToFrequency(String freqFileName) {
-        String outpath = outputPath();
         try {
-            System.out.println("Generating Frequency File");
-            FileWriter writer = new FileWriter(outpath + "/" + freqFileName);
-            for (Integer key : frequency.keySet()) {
+            FileWriter writer = new FileWriter(freqFileName);
+            for (Integer key : frequency.keySet()) { //Integer.toBinaryString() drops leading zeros
                 writer.write(String.format("%8s", Integer.toBinaryString(key)).replace(' ', '0')+":" +frequency.get(key)+ "\n");
+                //Converting 0-255 binary representation to binary, leading zero was dropped, we add them back
             }
             writer.close();
         } catch (IOException e) {
@@ -49,11 +45,10 @@ public class HuffmanMain implements Huffman {
 
     }
 
-    //Read the frequency file and put characters and frequencies into frequency map
+
     private void parseFrequencyFile(String frequencyFile) {
-        File file = new File(frequencyFile);
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedReader reader = new BufferedReader(new FileReader(frequencyFile));
             String line = reader.readLine();
             while (line != null) {
                 String[] arr = line.split(":");
@@ -67,14 +62,14 @@ public class HuffmanMain implements Huffman {
 
     }
 
-    //Enqueue element from a map into the instance variable queue of this huffmansubmit class
+
     private void enQueue() {
         for (Integer key : frequency.keySet()) {
             queue.offer(new HuffmanNode(key, frequency.get(key)));
         }
     }
 
-    //Nested HuffmanNode Class
+
     private class HuffmanNode implements Comparable<HuffmanNode> {
         int c; //unicode representation of the character
         int freq;
@@ -92,8 +87,7 @@ public class HuffmanMain implements Huffman {
         }
     }
 
-    //Build huffman tree with the priority queue instance variable.
-    //Return a HuffmanNode to the root of the tree.
+
     private HuffmanNode huffmanTree() {
         HuffmanNode root = null;
         while (queue.size() != 1) {
@@ -104,7 +98,7 @@ public class HuffmanMain implements Huffman {
             totalFrequency.left = minNode;
             totalFrequency.right = secondMinNode;
             root = totalFrequency;
-            queue.offer(root);
+            queue.offer(root); //Put root node back for next iteration comparsion
         }
         return root;
     }
@@ -114,7 +108,7 @@ public class HuffmanMain implements Huffman {
             return;
 
         if (node.left == null && node.right == null)
-            huffCodeMap.put(node.c, s);
+            huffCodeMap.put(node.c, s); //If reached leaf node, we made a huffman code, add it to hashmap
 
         huffmanCodeToMap(node.left, s + "0");
         huffmanCodeToMap(node.right, s + "1");
@@ -122,9 +116,7 @@ public class HuffmanMain implements Huffman {
 
 
     public void encode(String inputFile, String outputFile, String freqFile) {
-        String outpath = outputPath();
-        BinaryOut encodeStream = new BinaryOut(outpath + "/" + outputFile);
-        System.out.println("Output now writing");
+        BinaryOut encodeStream = new BinaryOut(outputFile);
         scanFrequency(inputFile);
         enQueue(); //enqueue element of frequency hashmap to pqueue
         writeToFrequency(freqFile);
@@ -138,6 +130,7 @@ public class HuffmanMain implements Huffman {
                 String [] arr = huffCodeMap.get(s).split("");
                 for(String i:arr){
                     encodeStream.write(i.equals("1"));
+                    totalOutputBits ++;
                 }
                 s = reader.read();
             }
@@ -145,19 +138,19 @@ public class HuffmanMain implements Huffman {
             e.printStackTrace();
         }
         encodeStream.flush();
-        queue.clear(); //Since we never poll() all element from queue, we must clear it after use
+        queue.clear();
+        System.out.println("File Encoded as " + outputFile);
     }
 
 
     public void decode(String inputFile, String outputFile, String freqFile) {
-        String outputpath = outputPath();
         parseFrequencyFile(freqFile);
         enQueue();
         HuffmanNode root = huffmanTree();
-        BinaryIn stream = new BinaryIn(inputFile);
+        BinaryIn stream = new BinaryIn("alice30.txt");
 
         try {
-            BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(outputpath + "/" + outputFile));
+            BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(outputFile));
             HuffmanNode current = root;
             while (!stream.isEmpty()) {
                 boolean b = stream.readBoolean();
@@ -167,6 +160,7 @@ public class HuffmanMain implements Huffman {
                     current = current.right;
                 }
                 if (current.left == null && current.right == null) {
+                    System.out.println(current.c);
                     writer.write(current.c);
                     current = root;
                 }
@@ -179,17 +173,7 @@ public class HuffmanMain implements Huffman {
         }
 
         queue.clear();
-    }
-
-    private String outputPath() {
-        try {
-            Process currDir = Runtime.getRuntime().exec("pwd");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(currDir.getInputStream()));
-            return reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Failed to find path to current directory. Output file directed to home");
-        }
-        return System.getProperty("user.home");
+        System.out.println("File Decoded ");
     }
 
     public static void main(String[] args) {
@@ -197,27 +181,26 @@ public class HuffmanMain implements Huffman {
         args[0] method of encode or decode
         args[1] encode input file name/ decode input file name
         args[2] encode output file name/ decode output file name
+        args[3] pwd current directory
         */
-
         HuffmanMain huffman = new HuffmanMain();
-        URL url = HuffmanMain.class.getResource(args[1]); //Full Absolute Path for input file
+        String pwd = args[args.length-1]+"/";
 
-        /*Manipulating output file name
+        /*
+        Manipulating output file name
         If args.length is 2, only two parameters are provided, therefore no output file name
         is specified, we parse the args[1], the default input file
 
         If args.length is 3, a specific output file name is requested, we parse args[2]
         */
+
         String parsedFile = args[1];
-
         if(args[0].equals("encode")){
-
-
-            if(args.length == 2 && args[1].contains(".")){ //If already have file definition
+            if(args.length == 3 && args[1].contains(".")){ //If already have file definition
                 parsedFile = args[1].substring(0, args[1].lastIndexOf('.'));
             }
 
-            else if(args.length ==3){ //If length = 3 we will parse the second argument
+            else if(args.length ==4){ //If length = 3 we will parse the second argument
                 parsedFile = args[2];
                 if (parsedFile.contains(".")){
                     parsedFile = parsedFile.substring(0, parsedFile.lastIndexOf('.'));
@@ -225,19 +208,18 @@ public class HuffmanMain implements Huffman {
             }
             String frequencyFileName = "."+ parsedFile + "_freq.txt"; //Add frequency to the parsed file to keep track of names
             String encodeFileName = parsedFile + ".enc"; //Add enc to all files
-            huffman.encode(url.getFile(), encodeFileName, frequencyFileName);
+            System.out.println(pwd+args[1]);
+            huffman.encode(pwd+args[1], pwd+encodeFileName, pwd+frequencyFileName);
         }
 
         else{ //decode
             parsedFile = args[1].substring(0, args[1].lastIndexOf('.'));
             String frequencyFileName = "."+parsedFile + "_freq.txt";
-            URL frequrl = HuffmanMain.class.getResource(frequencyFileName);
-
-            if(args.length ==3){
-                huffman.decode(url.getFile(), args[2], frequrl.getFile());
+            if(args.length ==4){
+                huffman.decode(pwd+args[1], pwd+args[2],pwd+frequencyFileName);
             }
             else{
-                // huffman.decode(url.getFile(),parsedFile,frequrl.getFile());
+                huffman.decode(pwd+args[1],pwd+parsedFile,pwd+frequencyFileName);
             }
         }
 
